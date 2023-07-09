2b62b2807231dd66fc23d9f4a4569d9e4198ef6f
Liladhar Varma <110517645+LiladharVarma@users.noreply.github.com>
2023-07-09 13:29:12 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -170,20 +170,6 @@ void displayWeight(int weight) {
     case -9:
     case -10:
     case -11:
-    case -12:
-     case -13:
-    case -14:
-    case -15:
-    case -16:
-    case -17:
-    case -18:
-    case -19:
-    case -20:
-    case -21:
-    case -22:
-    case -23:
-    case -24:
-
       {
         digitalWrite(a, HIGH);
         digitalWrite(b, HIGH);
